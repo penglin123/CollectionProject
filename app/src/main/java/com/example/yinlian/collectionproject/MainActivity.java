557b15f0054af9cc5ecb5808e951c401310e7487
penglin <1273395594@qq.com>
2018-04-09 22:10:28 +0800

    modify
    Signed-off-by: penglin <1273395594@qq.com>

--- app/src/main/java/com/example/yinlian/collectionproject/MainActivity.java
+++ app/src/main/java/com/example/yinlian/collectionproject/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.yinlian.collectionproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        text = (TextView) findViewById(R.id.text);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        });
+
     }
 }
